74fd58b85da5dd64e197a3865d72fb4dd843a9f2
student <student@158.197.35.239>
2020-09-23 12:02:45 +0200

    ejha

--- src/Prva.java
+++ src/Prva.java
@@ -3,5 +3,10 @@ public class Prva {
 	public static void main(String[] args) {
 		System.out.println("Dominik Madzik");
 		System.out.println("Dajaka zmena abo co");
+		System.out.println("Skákal pes,\r\n"
+				+ "pøes oves,¨\r\n"
+				+ "pøes zelenou louku.\r\n"
+				+ "Šel za ním myslivec,\r\n"
+				+ "péro na klobouku.");
 	}
 }
