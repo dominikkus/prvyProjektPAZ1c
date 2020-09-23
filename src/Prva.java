3bab1133f90fa8b9cd1d5b601da463ecd9d2a1c3
student <student@158.197.35.232>
2020-09-23 12:04:51 +0200

    obed v prvej triede

--- src/Prva.java
+++ src/Prva.java
@@ -3,5 +3,6 @@ public class Prva {
 	public static void main(String[] args) {
 		System.out.println("Dominik Madzik");
 		System.out.println("Dajaka zmena abo co");
+		System.out.println("preco nejdes na obed?");
 	}
 }
